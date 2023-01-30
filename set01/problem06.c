4d4c413e5a5002dcf971f175abe8003052a9c417
Saransh  Kumar <117524473+saransh710@users.noreply.github.com>
2023-01-30 05:40:26 +0000

    added other method

--- set01/problem06.c
+++ set01/problem06.c
@@ -20,3 +20,25 @@ int main() {
   
      return 0;
 }
+////proto#2
+#include <stdio.h>
+
+void compare(int a, int b, int c,int *largest) {
+     *largest = a;
+    if (b > *largest) {
+        *largest = b;
+    }
+    if (c > *largest) {
+        *largest = c;
+    }
+    printf("Largest number is: %d\n", *largest);
+}
+
+int main() {
+    int a, b, c,largest;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    compare( a, b, c,&largest);
+  
+     return 0;
+}
